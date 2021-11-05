f58320f73d56f6e02163e0981ccf2b5a4060f89d
Tani Kaye <1tanikaye@gmail.com>
2021-11-05 17:43:43 -0400

    commit tree ccc367c073b2533e053c6d479954e157ec57ef98

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit, newItemName, newItemCategory, items, input, formNameChange, categoryTypeChange}) {
+  const newItem = {
+    id: uuid(), // the `uuid` library can be used to generate a unique id
+    name: newItemName,
+    category: newItemCategory,
+  };
+
+  const arrayWithNewItem = [...items, newItem]
+  console.log("these are the new items", arrayWithNewItem)
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={onItemFormSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={input} onChange={formNameChange}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" onChange={categoryTypeChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </label>
 
-      <button type="submit">Add to List</button>
+      <button type="submit" >Add to List</button>
     </form>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,26 +2,72 @@ import React, { useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
+import { v4 as uuid } from "uuid";
 
-function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
 
-  function handleCategoryChange(event) {
-    setSelectedCategory(event.target.value);
+
+function ShoppingList({ items, setItems }) {
+  // const [selectedCategory, setSelectedCategory] = useState("All");
+  const [selectedName, setSelectedName] = useState("");
+  const [newItemName, setNewItemName] = useState("");
+  const [newItemCategory, setNewItemCategory] = useState("produce");
+  const [input, setInput] = useState('')
+  const [categoryType, setCategoryType] = useState('produce')
+
+
+
+  // function handleCategoryChange(event) {
+  //   setSelectedCategory(event.target.value);
+  // }
+
+  function handleNameChange (event) {
+    console.log('hello there')
+    setSelectedName(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  const newItem = {
+    id: uuid(), // the `uuid` library can be used to generate a unique id
+    name: input,
+    category: categoryType,
+  }
 
-    return item.category === selectedCategory;
+  function handleFormSubmission (event){
+    event.preventDefault()
+    console.log('form has been subbed');
+    console.log(event.target.input)
+    setItems([...items, newItem])
+
+    // setNewItemName(input)
+    // //console.log("yoyoyo", event.target.input.value)
+    // setNewItemCategory(categoryType)
+  }
+
+function formNameChange(e) {
+  setInput(e.target.value)
+}
+
+function categoryTypeChange(e) {
+  setCategoryType(e.target.value)
+}
+
+
+  // const itemsToDisplay = items.filter((item) => {
+  //   if (selectedCategory === "All") return true;
+
+  //   return item.category === selectedCategory;
+  // });
+
+  const newItemsToDisplay = items.filter((item) => {
+    if (selectedName === '') return true;
+    return item.name.toLowerCase().includes(selectedName.toLowerCase());
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={handleFormSubmission} newItemName={newItemName} newItemCategory={newItemCategory} items={items} input={input} formNameChange={formNameChange} categoryTypeChange={categoryTypeChange}/>
+      <Filter  onNameChange={handleNameChange}/>
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {newItemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
